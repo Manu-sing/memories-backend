22ed1bc8769e7914935d52dbfd4892322c6eab9d
Emanuele Guarnaccia <emanueleguarnaccia@gmail.com>
2022-05-29 12:00:03 +0800

    moved the user authorization in the middleware and exported it as a function to use in my routes

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,7 +2,8 @@ const postsRouter = require("express").Router();
 const Post = require("../models/post");
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
-const authorizeUser = require("../utils/middleware");
+const { auth } = require("../utils/middleware");
+const { request } = require("express");
 
 const getTokenFrom = (req) => {
   const authorization = req.get("authorization");
@@ -30,70 +31,77 @@ postsRouter.get("/:id", async (req, res, next) => {
     .catch((error) => next(error));
 });
 
-postsRouter.post("/", async (req, res, next) => {
-  const post = req.body;
-
-  const token = getTokenFrom(req);
-  const decodedToken = jwt.verify(token, process.env.SECRET);
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: "token missing or invalid" });
+postsRouter.post("/", auth, async (req, res, next) => {
+  try {
+    const post = req.body;
+    const user = await User.findById(req.user.id);
+    const newPost = new Post({ ...post, user: user._id });
+    const savedPost = await newPost.save();
+    user.posts = user.posts.concat(savedPost._id);
+    await user.save();
+    res.json(savedPost.toJSON());
+  } catch (error) {
+    next(error);
   }
-  const user = await User.findById(decodedToken.id);
-  const newPost = new Post({ ...post, user: user._id });
-  // we will pass in the value that we are receiving from the frontend form
-
-  newPost
-    .save()
-    .then((savedPost) => {
-      user.posts = user.posts.concat(savedPost._id);
-      user.save();
-      res.json(savedPost);
-    })
-    .catch((error) => next(error));
 });
 
-postsRouter.put("/:id", async (req, res, next) => {
+postsRouter.put("/:id", auth, async (req, res, next) => {
   const post = req.body;
-
-  const token = getTokenFrom(req);
-  const decodedToken = jwt.verify(token, process.env.SECRET);
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: "token missing or invalid" });
+  const authorization = req.get("authorization");
+  token = authorization.split(" ")[1];
+  const decoded = jwt.verify(token, process.env.SECRET);
+  try {
+    const postToEdit = await Post.findById(req.params.id);
+    if (postToEdit.user.toString() === decoded.id.toString()) {
+      Post.findByIdAndUpdate(req.params.id, post, { new: true }).then(
+        (updatedPost) => {
+          res.json(updatedPost);
+        }
+      );
+    }
+  } catch (error) {
+    next(error);
   }
-
-  const postToEdit = await Post.findById(req.params.id);
-  if (postToEdit.user.toString() === decodedToken.id.toString()) {
-    Post.findByIdAndUpdate(req.params.id, post, { new: true })
-      .then((updatedPost) => {
-        res.json(updatedPost);
-      })
-      .catch((error) => next(error));
-  }
-  // if (!mongoose.Types.ObjectId.isValid(id))
-  //   return response.status(404).send("No post with that id");
 });
 
-postsRouter.delete("/:id", async (req, res, next) => {
-  const token = getTokenFrom(req);
-  const decodedToken = jwt.verify(token, process.env.SECRET);
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: "token missing or invalid" });
+postsRouter.delete("/:id", auth, async (req, res, next) => {
+  const authorization = req.get("authorization");
+  token = authorization.split(" ")[1];
+  const decoded = jwt.verify(token, process.env.SECRET);
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.user.toString() === decoded.id.toString()) {
+      await User.findByIdAndUpdate(
+        { _id: post.user },
+        { $pull: { posts: post._id } }
+      );
+      await Post.findByIdAndRemove(req.params.id);
+      res.status(204).end();
+    }
+  } catch (error) {
+    next(error);
   }
 
-  const post = await Post.findById(req.params.id);
-  // const user = await User.findById(post.user);
+  // const token = getTokenFrom(req);
+  // const decodedToken = jwt.verify(token, process.env.SECRET);
+  // if (!decodedToken.id) {
+  //   return response.status(401).json({ error: "token missing or invalid" });
+  // }
 
-  if (post.user.toString() === decodedToken.id.toString()) {
-    await User.findByIdAndUpdate(
-      { _id: post.user },
-      { $pull: { posts: post._id } }
-    );
-    await Post.findByIdAndRemove(req.params.id)
-      .then(() => {
-        res.status(204).end();
-      })
-      .catch((error) => next(error));
-  }
+  // const post = await Post.findById(req.params.id);
+  // // const user = await User.findById(post.user);
+
+  // if (post.user.toString() === decodedToken.id.toString()) {
+  //   await User.findByIdAndUpdate(
+  //     { _id: post.user },
+  //     { $pull: { posts: post._id } }
+  //   );
+  //   await Post.findByIdAndRemove(req.params.id)
+  //     .then(() => {
+  //       res.status(204).end();
+  //     })
+  //     .catch((error) => next(error));
+  // }
 });
 
 module.exports = postsRouter;
--- utils/middleware.js
+++ utils/middleware.js
@@ -25,8 +25,29 @@ const unknownEndpoint = (req, res) => {
   res.status(404).send({ error: "unknown endpoint" });
 };
 
+const auth = (req, res, next) => {
+  let token;
+  const authorization = req.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    token = authorization.split(" ")[1];
+  }
+
+  if (!token) {
+    return res.status(401).json({ error: "token missing" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(400).json({ error: "token invalid" });
+  }
+};
+
 module.exports = {
   errorHandler,
   unknownEndpoint,
   requestLogger,
+  auth,
 };
